b6848b23b1302999b0836e0641948afafddf14da
Nani1979 <139854785+Nani1979@users.noreply.github.com>
2025-12-04 11:18:21 +0000

    Pending changes exported from your codespace

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,2 +1,5 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for HelloWorld
+print("hello world")
+name = input("what is your name? ")
+print("nice to meet you ", name)
--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -11,11 +11,22 @@ mybool = True
 
 # We can display the content of a variable using the print() function
 
-
+print("my integer is ",myint)
+print("my string is ",mystr)
 # Operators are used to perform operations on variables
-
-
+# print("my int added to my flot",myint + myfloat)
+# print("my int times my flot",myint * myfloat)
+# print("my int divided by my flot",myint / myfloat)
+# another_str = "this is another string"
+# print("string added", mystr + another_str)
+# print("nom " * 3)
 # Logical and comparison operators 
-
-
+# print(myint == 10)
+# print(myfloat != 20)
+# print(myint > 20)
+# print(myfloat < 10)
+# print(myint > 5 and myfloat <25.0)
+# print(not(myint > 5 and myfloat <25.0))
 # re-declaring a variable works
+myint = "abc"
+print("change int to string", myint)
